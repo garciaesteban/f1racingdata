6d5519b27ad4570a2409784f6728d7bf7e290531
Esteban Garcia <egarcia110495@gmail.com>
2022-04-27 20:30:37 -0500

    Created f1data app and created Season model

--- f1data/models.py
+++ f1data/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Season(models.Model):
+    year = models.IntegerField()
+    url = models.URLField(max_length=255)
+    races = models.ManyToManyField('Race')
+    qualifying = models.ManyToManyField('Qualifying')
+    results = models.ManyToManyField('Result')
+    sprintresults = models.ManyToManyField('Sprintresult')
+    drivers = models.ManyToManyField('Driver')
+    constructors = models.ManyToManyField('Constructor')
+    circuits = models.ManyToManyField('Circuit')
--- f1racingdata/settings.py
+++ f1racingdata/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'f1data.apps.F1DataConfig'
 ]
 
 MIDDLEWARE = [
