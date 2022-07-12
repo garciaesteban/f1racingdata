b166ba24d533766acbf0b5165934efa59b77618e
Esteban Garcia <egarcia110495@gmail.com>
2022-07-12 07:10:27 -0500

    created link to season detail

--- f1data/urls.py
+++ f1data/urls.py
@@ -5,4 +5,5 @@ app_name = "f1data"
 urlpatterns = [
     path('', views.home, name="home"),
     path('season/', views.season, name="season"),
+    path('season/<int:year>/', views.detail_season, name="detail_season"),
 ]
