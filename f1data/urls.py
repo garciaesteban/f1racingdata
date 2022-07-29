f38a5640901036c5caa33ce3deb3f2045d3f34e7
Esteban Garcia <egarcia110495@gmail.com>
2022-07-29 10:23:03 -0500

    created path for races and detail race

--- f1data/urls.py
+++ f1data/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('season/', views.season, name="season"),
     path('season/<int:year>/', views.detail_season, name="detail_season"),
+    path('races/', views.races, name="races"),
+    path('races/<int:race>/', views.detail_race, name="detail_race"),
 ]
